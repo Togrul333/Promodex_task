93f6ff4ea56e2de409397041c9aae4156de49404
Togrul <togrulshahbazov@gmail.com>
2022-11-29 14:44:21 +0400

    Update in Readme.md

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\NotificationRequest;
 use App\Http\Requests\SimpleActionRequest;
 use App\Lib\Repositories\NotificationRepository;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\Log;
 
 class NotificationController extends Controller
 {
@@ -26,6 +24,7 @@ class NotificationController extends Controller
         ];
         return view('notifications.index', $view_model);
     }
+
     public function create()
     {
         $view_model = [
@@ -45,24 +44,14 @@ class NotificationController extends Controller
     public function destroy(SimpleActionRequest $request)
     {
         $data = $request->validated();
-        try {
-            $this->notificationRepository->delete($data['id']);
-        } catch (ModelNotFoundException $e) {
-            Log::error($e->getMessage());
-            abort(404);
-        }
+        $this->notificationRepository->delete($data['id']);
         return redirect()->route('notifications')->with('info', 'уведомления удалены!');
     }
 
     public function changeStatus(SimpleActionRequest $request)
     {
         $data = $request->validated();
-        try {
-            $this->notificationRepository->change_status($data['id']);
-        } catch (ModelNotFoundException $e) {
-            Log::error($e->getMessage());
-            abort(404,'Ошибка!!');
-        }
+        $this->notificationRepository->change_status($data['id']);
         return redirect()->route('notifications')->with('info', 'Статус изменен!');
     }
 }
